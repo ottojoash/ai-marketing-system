f251110df30778c8b3aa04da7fd8b66524b22a63
ottojoash <louisjoshbricks@gmail.com>
2024-11-21 20:42:57 +0300

    dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,9 +4,6 @@ import React from "react";
 const Dashboard = () => {
   return (
     <div className="flex min-h-screen bg-gray-100">
-      {/* Sidebar */}
-      {/* <Sidebar/> */}
-
       {/* Main Content */}
       <main className="flex-1 p-4 md:p-6">
         {/* Header */}
