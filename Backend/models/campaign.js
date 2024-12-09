63be6e43ff3e819c7a8dc5a3aa415b9dd9871d25
ottojoash <louisjoshbricks@gmail.com>
2024-12-09 07:44:13 +0300

    compaign

---
+++ Backend/models/campaign.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const campaignSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // Reference to the User model
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  budget: {
+    type: Number,
+    required: true,
+  },
+  platforms: {
+    type: [String], // e.g., ["Facebook", "Instagram", "Google Ads"]
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Campaign", campaignSchema);
