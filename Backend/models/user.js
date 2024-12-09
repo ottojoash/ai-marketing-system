090b9f320840f3b02492cf116f4bcfa42050f4d9
ottojoash <louisjoshbricks@gmail.com>
2024-12-09 07:36:51 +0300

    auth login check

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -20,5 +21,18 @@ const userSchema = new mongoose.Schema({
     default: 'marketer', // Default role
   },
 });
+// Hash password before saving to the database
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare entered password with hashed password in the database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 
 module.exports = mongoose.model('User', userSchema);
